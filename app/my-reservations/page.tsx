1c85d24470b0b738cf2597402a5019c9bd1b5812
Paulos19 <paulohenrique.012araujo@gmail.com>
2025-06-09 09:36:39 -0300

    zaca

--- app/my-reservations/page.tsx
+++ app/my-reservations/page.tsx
@@ -1,7 +1,10 @@
+import Navbar from '../components/layout/Navbar';
 import { MyReservationsClient } from './components/MyReservationsClient';
 
 export default function MyReservationsPage() {
   return (
+    <>
+    <Navbar/>
     <div className="container mx-auto px-4 py-8">
       <div className="mb-8">
         <h1 className="text-4xl font-bold tracking-tight">Minhas Reservas</h1>
@@ -11,5 +14,6 @@ export default function MyReservationsPage() {
       </div>
       <MyReservationsClient />
     </div>
+    </>
   );
 }
